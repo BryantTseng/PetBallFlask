f686760372d851a9fe914d8cd36b5be9efd44189
Unknown <bryant5968@gmail.com>
2018-02-23 14:41:41 +0800

    add lines

--- pinsControl.py
+++ pinsControl.py
@@ -40,7 +40,7 @@ def data():
 def camera(command):
     if command =='start':
         #p = Popen(['ls'])
-        
+        p = Popen(['sudo', 'python', 'runstream.py'])
         #p = Popen(['raspvid', '-t', '999999', '-h', '1080', '-w', '1920', '-fps', '30', '-hf', '-b', '2000000', '-o', '-', '|', 'gst_launch-1.0', '-v', 'fdsrc', '!', 'h264parse', '!', 'rtph264pay', 'config-interval=1', 'pt=96', '!', 'gdppay', '!', 'tcpserversink', 'host=', ipaddress, 'port=5000')
         #os.system("raspivid -t 999999 -h 1080 -w 1920 -fps 30 -hf -b 2000000 -o - | gst-launch-1.0 -v fdsrc ! h264parse !  rtph264pay config-interval=1 pt=96 ! gdppay ! tcpserversink host= "+ipaddress+" port=5000")
     elif command == 'stop':
