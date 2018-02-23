5919f6250961fd646ba77a327d109e71d28202e5
Unknown <bryant5968@gmail.com>
2018-02-23 14:46:02 +0800

    add terminate

--- pinsControl.py
+++ pinsControl.py
@@ -44,6 +44,7 @@ def camera(command):
         #p = Popen(['raspvid', '-t', '999999', '-h', '1080', '-w', '1920', '-fps', '30', '-hf', '-b', '2000000', '-o', '-', '|', 'gst_launch-1.0', '-v', 'fdsrc', '!', 'h264parse', '!', 'rtph264pay', 'config-interval=1', 'pt=96', '!', 'gdppay', '!', 'tcpserversink', 'host=', ipaddress, 'port=5000')
         #os.system("raspivid -t 999999 -h 1080 -w 1920 -fps 30 -hf -b 2000000 -o - | gst-launch-1.0 -v fdsrc ! h264parse !  rtph264pay config-interval=1 pt=96 ! gdppay ! tcpserversink host= "+ipaddress+" port=5000")
     elif command == 'stop':
+        p.terminate()
         print('stop')
     elif command == 'capture':
         print('capture')
